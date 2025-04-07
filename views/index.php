cb1e0b9cad290c3f1f105cb9f6a56ff5262658f5
Lucas Ramalho <adryelll85@gmail.com>
2025-04-06 23:31:53 -0300

    First Commit

---
+++ views/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+    <link rel="stylesheet" type="text/css" href="../css/index.css">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <div class="logo">LOGO <span class="mesoName">Meso</span><span class="mindsName">Minds</span></div>
+            <a href="#">Conteúdos</a>
+            <a href="#">Questões</a>
+            <a href="#">Simulado</a>
+            <div class="loginButton">
+                <a href="#">Entrar</a>
+                <a href="#">Registrar-se</a>
+            </div>
+        </nav>
+    </header>
+
+    <div class="ComeceAgoraArea">
+        <h1>Comece a aprender matemática agora! Grátis!</h1>
+        <a href="#">Comece a aprender agora mesmo!</a>
+    </div>
+
+    <div class="bannerArea">
+        <img src="../imgs/dinobanner.jpg">
+    </div>
+
+    <div class="questoesContainer">
+    <h2>Teste seu conhecimento!</h2>
+    <div class="questoesContent"> 
+        <div class="questContArea">
+            <a href="#">Questões Matemática</a>
+            <a href="#">Questões Matemática</a>
+            <a href="#">Questões Matemática</a>
+            <a href="#">Questões Matemática</a>
+            <a href="#">Questões Matemática</a>
+        </div>
+        <div class="questEnemeArea">
+            <a href="#">Questões ENEM ANO</a>
+            <a href="#">Questões ENEM ANO</a>
+            <a href="#">Questões ENEM ANO</a>
+            <a href="#">Questões ENEM ANO</a>
+        </div>
+    </div>
+</div>
+
+    <div class="CadasAgoraArea">
+        <h1>
+            Comece a aprender agora!
+        </h1>
+        <a href="#">Comece a aprender!</a>
+    </div>
+
+    <footer>
+        <h1>footer</h1>
+    </footer>
+
+</body>
+
+</html>
